7d8d3392ae21e5a4e7b94ce2756c8b5d1a7e0322
nguyenthanhsang70 <layn7425@gmail.com>
2025-02-25 16:12:27 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Em à!',
+    introDesc: `Có một đốm lửa nhỏ
+                Thắp lên hạnh phúc to
+                Em đã có ai ngỏ?
+                Hay là để anh lo?`,
+    btnIntro: 'hihihi',
+    title: ' Em có thích anh không? ',
+    desc: 'Em có bản đồ không? Anh bị lạc trong ánh mắt em rồi. ',
+    btnYes: 'Thích anh thì bấm đây!',
+    btnNo: 'Không thích anh thì bấm đây!',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại chọn anh ',
+    btnReply: 'Gửi cho anh ',
+    reply: 'Vì trong hàng tỉ người đó, chỉ có anh đủ đặc biệt với em',
+    mess: 'Số nhọ bốc trúng anh =)))',
+    messDesc: 'Tối nay anh qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
